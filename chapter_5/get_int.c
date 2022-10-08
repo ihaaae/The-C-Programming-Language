a5fb63e133cc9d55147031c3cf968da60399e574
liuxch <5929624d@gmailcom>
2022-10-08 21:54:41 +0800

    5.1 and 5.2

--- chapter_5/get_int.c
+++ chapter_5/get_int.c
@@ -46,5 +46,49 @@ int getint_fixed(int *pn)
 
 int getfloat(float *pn)
 {
+    int c;
+    float sign, power = 1.0;
+    while(isspace(c = getch())) {
+        ;
+    }
+    if(!isdigit(c) && c != '+' && c != '-' && c != EOF) {
+        ungetch(c);
+        return 0;
+    }
 
+    sign = (c == '-') ? -1.0 : 1.0;
+
+    if(c == '+' || c == '-') {
+        c = getch();
+    }
+    else if(isdigit(c)) {
+        *pn = c - '0';
+    }
+    else {
+        return 0;
+    }
+
+    for(;isdigit(c); c = getch()) {
+        *pn = *pn * 10.0 + (c - '0');
+    }
+
+    if(c == '.') {
+        c = getch();
+    }
+    else {
+        return c;
+    }
+
+    for(; isdigit(c); c = getch()) {
+        *pn = *pn * 10.0 + (c - '0');
+        power *= 10.0;
+    }
+
+    *pn = *pn / power * sign;
+
+    if(c != EOF) {
+        ungetch(c);
+    }
+
+    return c;
 }
